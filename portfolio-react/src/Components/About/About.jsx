847c053ac636bda1bcd104d22b7ddde365ac7d2c
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-04-24 20:35:27 +0530

    creating About component

---
+++ portfolio-react/src/Components/About/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.svg'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            <img src={theme_pattern} alt=""/>
+        </div>
+        <div className="about-sections">
+            <div className="about-left">
+                <img src={profile_img} alt="" />
+            </div>
+            <div className="about-right">
+                <div className="about-para">
+                    <p>I,m experienced Frontend Developer </p>
+                </div>
+            </div>
+        </div>  
+    </div>
+  )
+}
+
+export default About
