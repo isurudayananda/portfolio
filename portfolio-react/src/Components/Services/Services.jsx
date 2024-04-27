714dcad35dc8a449a5a9b0b53ebf8feab63fedd2
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-04-27 10:27:27 +0530

    creating Sevices component CSS properties

--- portfolio-react/src/Components/Services/Services.jsx
+++ portfolio-react/src/Components/Services/Services.jsx
@@ -13,13 +13,13 @@ const Services = () => {
         </div>
         <div className="services-container">
             {Services_Data.map((service,index)=>{
-                return <div key={index} className='service-format'>
+                return <div key={index} className='services-format'>
                     <h3>{service.s_no}</h3>
                     <h2>{service.s_name}</h2>
                     <p>{service.s_desc}</p>
                     <div className='services-readmore'>
                         <p>Read More</p>
-                        <img src="" alt="" />
+                        <img src={arrow_icon} alt="" />
                     </div>
                 </div>
             })}
