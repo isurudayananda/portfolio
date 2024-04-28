1699802bcc88e9f84505a6f9e952bf705d74d47b
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-04-28 14:15:04 +0530

    updating navbar.jsx

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
+import { useState, React } from 'react'
+import underline from '../../assets/nav_underline.svg'
 
 const Navbar = () => {
+
+  const [menu,setMenu]  = useState("home");
+
   return (
     <div className='navbar'>
       <img src={logo}  alt=""/>
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li><p>Home</p></li>
+        <li><p>About Me</p></li>
+        <li><p>Services</p></li>
+        <li><p>Portfolio</p></li>
+        <li><p>Contact</p></li>
       </ul>
     <div className="nav-connect">Connect With Me</div>
     </div>
