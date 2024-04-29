fedd57248ccb4cdb74c7c1e5f4ea6c9826468ae2
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-04-29 23:04:12 +0530

    add onclick function in navbar

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={logo}  alt=""/>
       <ul className="nav-menu">
-        <li><p>Home</p></li>
-        <li><p>About Me</p></li>
-        <li><p>Services</p></li>
-        <li><p>Portfolio</p></li>
-        <li><p>Contact</p></li>
+        <li><p onClick={()=>setMenu("home")}>Home</p>{menu==="home"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("about")}>About Me</p>{menu==="about"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("services")}>Services</p>{menu==="services"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("work")}>Portfolio</p>{menu==="work"?<img src={underline} alt=''/>:<></>}</li>
+        <li><p onClick={()=>setMenu("contact")}>Contact</p>{menu==="contact"?<img src={underline} alt=''/>:<></>}</li>
       </ul>
     <div className="nav-connect">Connect With Me</div>
     </div>
