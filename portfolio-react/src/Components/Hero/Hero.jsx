4f60432de0c8f5ed7e6d78794a9f7e4215735836
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2024-05-06 02:26:51 +0530

    changing  hero images

--- portfolio-react/src/Components/Hero/Hero.jsx
+++ portfolio-react/src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img} alt=""  />
-        <h1><span>I'm ISuru Dayananda,</span> Frontend Developer, Undergraduate</h1>
+        <h1><span>I'm Isuru Dayananda,</span> Frontend Developer, Undergraduate</h1>
         <p>I am a Fronend developer </p>
         <div className="hero-action">
           <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
